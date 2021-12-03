720e93aa2d64e68f0837ab81638be78d3fd70ec4
toshyss <toshy.ss@gmail.com>
2021-12-04 00:29:34 +0900

    first commit

---
+++ src/test/java/com/example/spring_restservice/SpringRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
